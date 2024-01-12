96aafdbaa719480461ab679041bd74860d042039
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-01-12 12:15:42 +0530

    Access and refresh token setup

---
+++ src/models/user.models.js
@@ -0,0 +1,81 @@
+import mongoose, {Schema} from mongoose
+import bcrypt from 'bcrypt'
+const userSchema = new Schema({
+    userName:{
+        type:String,
+        requried:true,
+        trim:true,
+        lowercase:true,
+        unique:true,
+        index:true
+    },
+    email:{
+        type:String,
+        requried:true,
+        trim:true,
+        lowercase:true,
+        unique:true,
+    },
+    fullName:{
+        type:String,
+        trim:true,
+        unique:true,
+        index:true
+    },
+    avatar:{
+        type:String, // cloudinary URL
+        requried:true,
+    },
+    coverImage:{
+        type:String,
+    },
+    watchHistory:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type: String,
+        required:[true, 'Password is required']
+    },
+    refreshToken:{
+        type:String
+    }
+}, {timestamps:true})
+
+userSchema.pre('save', async function (next){
+    if(!this.isModified('password')) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = async function() {
+    return jwt.sign(
+    {
+     _id:this._id,
+     email : this.email,
+     username:this.userName
+    },
+    process.env.ACCESS_TOKEN_SECRET, 
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function() {
+    return jwt.sign(
+    {
+        _id:this._id,
+        email : this.email,
+        username:this.userName
+    },
+    process.env.REFRESH_TOKEN_SECRET, 
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
+
+export const User = mongoose.model('User', userSchema)
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,10 @@ const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (error) {
-    console.log("Error", error);
+    res.status(err.code || 500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 export { asyncHandler };
